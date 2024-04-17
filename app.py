8dbd3b851937353c3b66136c7805cdf29b9ee4b3
Daniel <dev.candidosivla@gmail.com>
2024-04-17 09:07:58 -0300

    Atualização do código do app usando kivy

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
 from kivy.app import App
-from kivy.uix.button import Button
+from kivy.uix button import BoxLayout
+from kivy.uix boxlayout import boxlayout
+from kivy.uix label import label
 
-class Test(App):
+class test(app)
     def build(self):
-        return Button(text='Tchoi', color='red')
+        box = BoxLayout (orientation = 'vertical')
+        button = button (text= 'botao 1')
+        label = label (text= 'Texto 1')
+        box.add_widget (button)
+        box.add_widget (label)
 
+        box2 = BoxLayout()
+        button2 = button (text = 'Botao 2')
+        label2 = label (text = 'Texto 2')
+        box2.add_widget (button2)
+        box2.add_widget (label2)
+
+        box.add_widget (box2)
+        return box
+    
 Test().run()
